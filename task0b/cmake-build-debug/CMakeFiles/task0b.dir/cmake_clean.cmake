0e8292271d6de254ee359c42c411a97a44b8dd33
Ivan Molochev <molo4evan@gmail.com>
2017-09-18 14:23:43 +0700

    please an it work

---
+++ task0b/cmake-build-debug/CMakeFiles/task0b.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task0b.dir/main.cpp.obj"
+  "CMakeFiles/task0b.dir/sort_strings.cpp.obj"
+  "CMakeFiles/task0b.dir/ololo.cpp.obj"
+  "task0b.pdb"
+  "task0b.exe"
+  "task0b.exe.manifest"
+  "libtask0b.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task0b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ task0b/main.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+#include "sort_strings.h"
+
+using namespace myNameSpace;
+using namespace std;
+
+int main(){
+    ifstream in("in.txt");
+    ofstream out("out.txt");
+    list<string> list_to_sort;
+
+    string tmp_str;
+    while (getline(in, tmp_str))
+    {
+        list_to_sort.push_back(tmp_str);
+    }
+
+    sort_strings(list_to_sort);
+
+    for(const auto& a: list_to_sort)
+    {
+        out << a << endl;
+    }
+    return 0;
+}
