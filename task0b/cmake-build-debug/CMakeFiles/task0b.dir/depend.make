7df2aebc87132f49782fe1e1df691ce7be57fc0c
Ivan Molochev <molo4evan@gmail.com>
2017-09-20 17:42:33 +0700

    #

--- task0b/cmake-build-debug/CMakeFiles/task0b.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for task0b.
-# This may be replaced when dependencies are built.
--- task0b/sort_strings.cpp
+++
@@ -1,7 +0,0 @@
-#include "sort_strings.h"
-
-namespace myNameSpace{
-    void sort_strings(list<string>& strings){
-        strings.sort();
-    }
-}
